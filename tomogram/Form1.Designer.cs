e4299d83242bfc207f87635a3db6d79deced0dcb
AlexanderVladislavovich <alexander7110230@outlook.com>
2023-04-24 18:19:45 +0300

    Добавьте файлы проекта.

---
+++ tomogram/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace tomogram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.glControl2 = new OpenTK.GLControl();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.menuStrip3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(1102, 28);
+            this.menuStrip3.TabIndex = 0;
+            this.menuStrip3.Text = "menuStrip3";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(59, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // glControl2
+            // 
+            this.glControl2.BackColor = System.Drawing.Color.Black;
+            this.glControl2.Location = new System.Drawing.Point(13, 34);
+            this.glControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glControl2.Name = "glControl2";
+            this.glControl2.Size = new System.Drawing.Size(852, 483);
+            this.glControl2.TabIndex = 1;
+            this.glControl2.VSync = false;
+            this.glControl2.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl2_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(872, 41);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(230, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(872, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "-layer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(989, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "+layer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(872, 125);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(111, 56);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(989, 125);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(113, 56);
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1102, 530);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.glControl2);
+            this.Controls.Add(this.menuStrip3);
+            this.MainMenuStrip = this.menuStrip3;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.MenuStrip menuStrip1;
+        //private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        //private OpenTK.GLControl glControl1;
+        //private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private OpenTK.GLControl glControl2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+    }
+}
+
---
+++ tomogram/Form1.cs
@@ -0,0 +1,254 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Drawing.Imaging;
+//using System.IO;
+//using System.Windows.Forms;
+//using OpenTK;
+//using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing.Imaging;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+
+namespace tomogram
+{
+    public partial class Form1 : Form
+    {
+        int curretLayer = 0;
+        int min = 0; int max= 2000;
+        Bin Bin = new Bin();
+        view view = new view();
+        bool loaded = false;
+        int FrameCount;
+        DateTime NextFPSUpdate = DateTime.Now.AddSeconds(1);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+        //    OpenFileDialog dialog = new OpenFileDialog();
+        //    if (dialog.ShowDialog() == DialogResult.OK)
+        //    {
+        //        string str = dialog.FileName;
+        //        Bin.readBIN(str);
+        //        view.SetupView(Width, Height);
+        //        view.SetupView(glControl1.Width, glControl1.Height);
+        //        loaded = true;
+        //        glControl1.Invalidate();
+        //    }
+        //}
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string str = dialog.FileName;
+                Bin.readBIN(str);
+                trackBar1.Maximum = Bin.Z - 1;
+                trackBar2.Minimum = 0;
+                trackBar2.Maximum = 500;
+                trackBar3.Minimum = 0;
+                trackBar3.Maximum = 5000;
+                //view.SetupView(Width, Height);
+                view.SetupView(glControl2.Width, glControl2.Height);
+                loaded = true;
+                glControl2.Invalidate();
+            }
+        }
+
+        private void glControl2_Paint(object sender, PaintEventArgs e)
+        {
+
+            if(true)
+            {
+                view.DrawQuads(curretLayer, min, max);
+                glControl2.SwapBuffers();
+            }
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            curretLayer = trackBar1.Value;
+            glControl2.Invalidate();
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            while (glControl2.IsIdle)
+            {
+                DisplayFPS();
+                glControl2.Invalidate();
+            }
+        }
+
+        void DisplayFPS()
+        {
+            if (DateTime.Now >= NextFPSUpdate)
+            {
+                this.Text = string.Format("CT Visualiser (fps={0})", FrameCount);
+                NextFPSUpdate = DateTime.Now.AddSeconds(1);
+                FrameCount = 0;
+            }
+            FrameCount++;
+        }
+
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( curretLayer >0 ) curretLayer -= 1;
+            glControl2.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ( curretLayer < Bin.Z - 1) curretLayer += 1;
+            glControl2.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Idle += Application_Idle;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            min = trackBar2.Value;
+            glControl2.Invalidate();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            max = trackBar3.Value;
+            glControl2.Invalidate();
+        }
+
+        //private void Application_Idle1(object sender, EventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void fffToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+
+        //}
+    }
+
+    class Bin
+    {
+        public static int X, Y, Z;
+        public static short[] array;
+        public Bin() { }
+
+        public static void readBIN(string path)
+        {
+            if (File.Exists(path))
+            {
+                BinaryReader reader =
+                    new BinaryReader(File.Open(path, FileMode.Open));
+
+                X = reader.ReadInt32();
+                Y = reader.ReadInt32();
+                Z = reader.ReadInt32();
+                int arraySize = X * Y * Z;
+                array = new short[arraySize];
+
+                for (int i = 0; i < arraySize; ++i)
+                {
+                    array[i] = reader.ReadInt16();
+                }
+            }
+        }
+    }
+
+    class view
+    {
+        public view() { }
+        public static void SetupView(int width, int height)
+        {
+            GL.ShadeModel(ShadingModel.Smooth);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+            GL.Viewport(0, 0, width, height);
+        }
+
+        //public static int clamp(int val, int min, int max)
+        //{
+        //    if (val <= min) return min;
+        //    else if (val >= max) return max;
+        //    else return val;
+        //}
+
+        public int clamp(int val, int min, int max)
+        {
+            if (val < min)
+                return min;
+            if (val > max)
+                return max;
+            return val;
+        }
+        Color TransferFunction(short value, int _min, int top)
+        {
+            int min = _min; int max = _min + top;
+            int newVal = clamp((value - min) * 255 / (max - min), 0, 255);
+            return Color.FromArgb(255, newVal, newVal, newVal);
+            //return Color.FromArgb(255, 200, 100, 150);
+        }
+        public void DrawQuads(int LayerNumber, int _min, int top)
+        {
+           
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Begin(PrimitiveType.Quads);
+            for (int x_coord = 0; x_coord < Bin.X - 1; x_coord++)
+            {
+                for (int y_coord = 0; y_coord < Bin.Y - 1; y_coord++)
+                {
+                    short value;
+                    value = Bin.array[x_coord + y_coord * Bin.X + LayerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value, _min, top));
+                    GL.Vertex2(x_coord, y_coord);
+
+                    value = Bin.array[x_coord + (y_coord + 1) * Bin.X + LayerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value, _min, top));
+                    GL.Vertex2(x_coord, y_coord + 1);
+
+                    value = Bin.array[x_coord + 1 + (y_coord + 1) * Bin.X + LayerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value, _min, top));
+                    GL.Vertex2(x_coord + 1, y_coord + 1);
+
+                    value = Bin.array[x_coord + 1 + y_coord * Bin.X + LayerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value, _min, top));
+                    GL.Vertex2(x_coord + 1, y_coord);
+                }
+            }
+            GL.End();
+        }
+    }
+}
